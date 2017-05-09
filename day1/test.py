870384d387d4ad912592ccf0f7e0f1853e857b62
Peninah Anyango <pennie.yna@gmail.com>
2017-05-09 11:59:42 +0300

    Final edits

--- day1/test.py
+++ day1/test.py
@@ -1,23 +1,26 @@
 """ Tests our prime_numbers """
 
 import unittest
+from unittest import TestCase
 from prime_numbers import prime
 
-class Test_prime_numbers (unittest.Testcase):
+class Test_prime_numbers (unittest.TestCase):
 
-	def test_value_is_number():
+	def test_value_is_number(self):
 		self.asserEqual (self.prime ("one"), "Sorry! Only numbers allowed")
 
-	def test_number_negatives():
+	def test_number_negatives(self):
 		self.assertEqual (self.prime (-1), "Sorry! Prime numbers are positives")
 
-	def test_number_positives():
+	def test_number_positives(self):
 		self.assertEqual (self.prime (3), True)
 		self.assertEqual (self.prime (4), False)
 
-	def test_number_is_whole():
+	def test_number_is_whole(self):
 		self.assertEqual (self.prime (float), "Sorry! Prime numbers are whole")
 		self.assertEqual (self.prime (3.2), "Sorry! Prime numbers are whole")
+
+
 
 		
 
