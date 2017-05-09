5f181b905e0f03864daebe41d0a3bdf5fda1f983
Peninah Anyango <pennie.yna@gmail.com>
2017-05-09 14:47:24 +0300

    Submit Car assignment

---
+++ day2/car.py
@@ -0,0 +1,48 @@
+class Car (object):
+  
+
+  def __init__ (self, name = None, model = None, vehicle_type = None):
+
+    self.name = name
+    self.model = model
+    self.vehicle_type = vehicle_type
+    self.speed = 0
+
+
+    if self.name is None:
+      return "General"
+    else:
+      return self.name
+
+    if self.model is None:
+      return "GM" 
+    else:
+      return self.model
+
+
+    if self.name is 'Porshe'or 'Koenigsegg':
+      self.num_of_doors = 2
+    else:
+      self.num_of_doors = 4
+
+    if self.vehicle_type is 'Trailer':
+      self.num_of_wheels = 8
+    else:
+      self.num_of_wheels = 4
+
+
+  def is_saloon(self):
+    if self.vehicle_type == saloon:
+      return True
+    elif self.vehicle_type != saloon:
+      return "Trailer"
+    else:
+      return False
+
+  def drive(self, moving_speed):
+    if moving_speed == 3:
+      self.speed = 1000
+    elif moving_speed == 7:
+      self.speed = 77
+
+    return self
