36b71d8d72d1226e593c4a9088f2c91484459247
Peninah Anyango <pennie.yna@gmail.com>
2017-05-11 15:11:50 +0300

    Edited Binary Search Assignment

--- day4/binary_search.py
+++ day4/binary_search.py
@@ -2,7 +2,8 @@ class BinarySearch(list):
     def __init__(self, a, b):
         self.a = a
         self.b = b
-        self.array = list.__init__(self, [i for i in range(self.b, (self.a + self.b), self.b) if i]) #returns list
+        #self.array = list.__init__(self, [i for i in range(self.b, (self.a + self.b), self.b) if i]) #returns list
+        self.array = list.__init__ (self, [i for i in range (self.b, (self.a + 1)) if i])
         self.length = len(self)
 
     def search(self,item):
@@ -10,23 +11,21 @@ class BinarySearch(list):
         count = 0
         answer ['count'] = 0
         first = 0       
-        last = self.length - 1
+        last = self.length + 1
         found = False
 
-        while first < last:
+        while first < last:                     
             mid = (first + last) // 2
             mid_val = mid
-
-         
             answer['count'] += 1
 
             if mid_val == item:
-                answer['index'] = mid
+                answer['index'] = mid - 1
                 found = True
                 return answer
 
             else:
-                if mid_val < item:
+                if mid_val <= item:
                     first = mid + 1
                 elif mid_val > item:
                     last = mid - 1
@@ -41,7 +40,7 @@ class BinarySearch(list):
 
 bin = BinarySearch(40,2)
 
-print (bin.search(33))
+print (bin.search(20))
 
 
        
